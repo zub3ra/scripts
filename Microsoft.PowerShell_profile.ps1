3b84e3435fcb8bf5c4ae6fa711e64366fa73d32a
Art Mills <art@hamcois.com>
2013-03-01 09:44:19 -0600

    Adding Powershell Cookbook by Lee Holmes scripts

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+ Set-StrictMode -Version 3
+ 
+ function Prompt 
+ {
+    $id = 1
+    $historyItem = Get-History -Count 1
+    if($historyItem)
+    {
+        $id = $historyItem.Id + 1 
+    }
+    
+    Write-Host -ForegroundColor DarkGray "`n[$(Get-Location)]"
+    Write-Host -NoNewLine "PS: $id > "
+    $host.UI.RawUI.WindowTitle = "$(Get-Location)"
+    
+    "`b" 
+    }
+
+Set-Alias new New-Object
+Set-Alias iexplore 'C:\Program Files\Internet Explorer\iexplore.exe'
